6b113b7d04e169495b2f20abfe687bb30bd4ebfa
Lê Thị Ngọc Yến <lethingocyen@MacBook-Pro-cua-Le-2.local>
2022-10-25 09:46:24 +0700

    reset to pull  request

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-
-
 ### Deployment
+
 Deploy lên heroku:
+
 - Tạo tài khoản trong heroku
 - Đăng nhập bằng câu lệnh `heroku login`
 - Tạo app trong heroku bằng câu lệnh `heroku create <tên app>`. Nếu app vừa tạo chưa liên kết với git thì thực hiện câu lệnh `git remote add heroku <tên app>`
@@ -12,3 +12,5 @@ Trang web: https://my-doctor-web.herokuapp.com/
 Dùng vercel:
 Link Web: https://my-doctor.vercel.app/
 
+update read
+and me
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { initializeFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+//lan1
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC3nFrxLvpnHbhEQmgpYSZJCntjXsLzaFI",
@@ -11,12 +11,18 @@ const firebaseConfig = {
   projectId: "my-doctor-91624",
   storageBucket: "my-doctor-91624.appspot.com",
   messagingSenderId: "277901999857",
-  appId: "1:277901999857:web:c84a69bbe6eea0204e7350"
+  appId: "1:277901999857:web:c84a69bbe6eea0204e7350",
 };
-
+//lan2
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const db = getFirestore(app);
-const db = initializeFirestore(app, {experimentalForceLongPolling: true});
+const db = initializeFirestore(app, { experimentalForceLongPolling: true });
 const storage = getStorage();
-export {db, storage };
+export { db, storage };
+//lan3
+//lan4
+//lan 5
+//lan6
+//lan7
+//lan8
